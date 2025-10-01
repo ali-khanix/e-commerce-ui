94b8fd9c8a03f355698ad7612884950e938777df
Ali Khani <khaniali568@gmail.com>
2025-10-01 20:30:00 +0330

    categories styles

--- client/src/components/Categories.tsx
+++ client/src/components/Categories.tsx
@@ -63,7 +63,7 @@ const Categories = () => {
   function handleChange(value: string | null) {
     const params = new URLSearchParams(searchParams);
     params.set("category", value || "all");
-    router.push(`${pathname}?${params.toString()}`);
+    router.push(`${pathname}?${params.toString()}`, { scroll: false });
   }
 
   return (
--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import Link from "next/link";
 import { ProductType } from "./types";
+import Image from "next/image";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
-  return <div>ProductCard</div>;
+  return (
+    <div className="shadow-lg rounded-lg overflow-hidden">
+      <Link href={`/products/${product.id}`}>
+        <div className="relative aspect-[2/3]">
+          <Image
+            src={product.images[product.colors[0]]}
+            alt={product.name}
+            fill
+            className="object-cover hover:scale-105 transition-all duration-300"
+          />
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default ProductCard;
