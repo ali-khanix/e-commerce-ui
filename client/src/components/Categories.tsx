af1ff3496989e2be0c93cacb4effc0843f4553c7
Ali Khani <khaniali568@gmail.com>
2025-10-02 19:22:58 +0330

    test

--- client/src/components/Categories.tsx
+++ client/src/components/Categories.tsx
@@ -9,7 +9,7 @@ import {
   Hand,
   Venus,
 } from "lucide-react";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+// import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const categories = [
   {
@@ -55,26 +55,26 @@ const categories = [
 ];
 
 const Categories = () => {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const pathname = usePathname();
-  const selectedCategory = searchParams.get("category");
-  console.log(selectedCategory);
+  // const searchParams = useSearchParams();
+  // const router = useRouter();
+  // const pathname = usePathname();
+  // const selectedCategory = searchParams.get("category");
+  // console.log(selectedCategory);
 
-  function handleChange(value: string | null) {
-    const params = new URLSearchParams(searchParams);
-    params.set("category", value || "all");
-    router.push(`${pathname}?${params.toString()}`, { scroll: false });
-  }
+  // function handleChange(value: string | null) {
+  //   const params = new URLSearchParams(searchParams);
+  //   params.set("category", value || "all");
+  //   router.push(`${pathname}?${params.toString()}`, { scroll: false });
+  // }
   return (
     <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 p-2 rounded-lg mb-4 text-sm">
       {categories.map((category) => (
         <div
           className={`flex items-center justify-center cursor-pointer gap-2 px-2 py-1 rounded-md ${
-            category.slug === selectedCategory ? "bg-white" : "text-gray-500"
+            category.slug === `selectedCategory` ? "bg-white" : "text-gray-500"
           }`}
           key={category.name}
-          onClick={() => handleChange(category.slug)}
+          // onClick={() => handleChange(category.slug)}
         >
           {category.icon}
           {category.name}
