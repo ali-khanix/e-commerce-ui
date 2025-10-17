fb5748292794e6548a20c2824953368e72df2c57
Ali Khani <103037526+ali-khanix@users.noreply.github.com>
2025-10-17 15:39:35 +0330

    cart details styles

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -89,21 +89,24 @@ const CartPage = () => {
       <h1 className="text-2xl font-medium">Your Shopping Cart</h1>
 
       {/* STEPS */}
-      <div className="flex flex-col lg:flex-row gap-12">
+      <div className="flex flex-col lg:flex-row items-center gap-8 lg:gap-16">
         {steps.map((step) => (
           <div
             key={step.id}
-            className={`flex items-center justify-start lg:justify-center gap-4 border-b-2 pb-4 ${
-              step.id === activeStep ? "border-gray-800" : "border-gray-400"
+            className={`flex items-center justify-start lg:justify-center gap-2 border-b-2 pb-4 ${
+              step.id === activeStep ? "border-gray-800" : "border-gray-200"
             }`}
           >
+            {/* STEP ID */}
             <div
-              className={`w-6 h-6 rounded-full flex items-center justify-center font-medium text-gray-200 text-sm ${
+              className={`w-6 h-6 rounded-full flex items-center justify-center font-medium text-white text-sm ${
                 step.id === activeStep ? "bg-gray-800" : "bg-gray-400"
               }`}
             >
               {step.id}
             </div>
+
+            {/* STEP TITLE */}
             <p
               className={`text-sm font-medium ${
                 step.id === activeStep ? "text-gray-800" : "text-gray-400"
@@ -115,50 +118,13 @@ const CartPage = () => {
         ))}
       </div>
 
-      {/* CART ITEMS & DETAILS */}
+      {/* STEPS & DETAILS */}
       <div className="w-full flex flex-col lg:flex-row gap-16">
-        {/* CART ITEMS */}
-        <div className="w-full lg:w-7/12 shadow-lg border-2 border-gray-100 rounded-lg flex flex-col gap-8">
-          <h2>Cart Items</h2>
+        {/* STEPS */}
+        <div className="cartSteps w-7/12">1</div>
 
-          {cartItems.map((item) => (
-            <div className="flex items-center justify-between" key={item.id}>
-              <Image
-                src={item.images.gray}
-                alt={item.name}
-                width={64}
-                height={64}
-              />
-
-              <div className="flex flex-col">
-                <h3>{item.name}</h3>
-                <span>{item.quantity}</span>
-                <span>{item.selectedSize}</span>
-                <span>{item.selectedColor}</span>
-                <span>{item.price}</span>
-              </div>
-
-              <RecycleIcon />
-            </div>
-          ))}
-        </div>
-
-        {/* CART DETAILS */}
-        <div className="w-full lg:w-5/12 shadow-lg border-2 border-gray-100 rounded-lg flex flex-col gap-8 p-8">
-          <h2>Cart Details</h2>
-          <div className="flex justify-between text-sm">
-            <p className="text-gray-500">Subtotal</p>
-            <p className="font-medium">
-              $
-              {cartItems
-                .reduce((acc, item) => acc + item.price * item.quantity, 0)
-                .toFixed(2)}
-            </p>
-          </div>
-          <button className="w-full bg-gray-800 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center gap-2 hover:bg-gray-900 transition-all duration-300">
-            Continue <ArrowRight id="arrow-right" className="w-3 h-3" />
-          </button>
-        </div>
+        {/* DETAILS */}
+        <div className="cartSteps w-5/12">2</div>
       </div>
     </div>
   );
